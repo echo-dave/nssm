b8054ff44114e4b88f7e08e2b1907111fd29f831
Dave May <echo.dave@protonmail.com>
2023-10-28 03:08:27 -0400

    prettier formattered added prettierrc

--- csv.js
+++ csv.js
@@ -1,36 +1,41 @@
-import { spawn } from 'node:child_process';
-const ps = spawn('ps', ['-axcmo', '%cpu,%mem,pid,time,user,comm']);
-const head = spawn('head', ["-n", "10"]);
-const awk = spawn('awk', [`{print $1"\t"$2"\t"$3"\t"$4"\t"$5" "$6" "$7" "$8}`]);
-import { log } from 'node:console';
+import { spawn } from 'node:child_process'
+const ps = spawn('ps', ['-axcmo', '%cpu,%mem,pid,time,user,comm'])
+const head = spawn('head', ['-n', '10'])
+const awk = spawn('awk', [`{print $1"\t"$2"\t"$3"\t"$4"\t"$5" "$6" "$7" "$8}`])
+import { log } from 'node:console'
 
 function processList(sortProcess) {
-    const processSort = sortProcess === "cpu" ? spawn('scripts/psCPU.bash') : spawn('scripts/psMem.bash');
-    processSort.stdout.on('data', data => {
-        data = data.toString()
-        data = data.split('\n')    
-    const labels = ["cpu", "mem", "pid", "time", "user", "process"]
-    let dbObject=[]
-    for (let i = 0; i < data.length-1; i++) {
-        data[i] = data[i].split(',')
-        let buildArray = []
-        for (let j = 0; j < data[i].length; j++) {
-            buildArray.push({[labels[j]]: data[i][j]})
-        }
-        dbObject.push(buildArray)
-    }; 
+  const processSort =
+    sortProcess === 'cpu'
+      ? spawn('scripts/psCPU.bash')
+      : spawn('scripts/psMem.bash')
+  processSort.stdout.on('data', (data) => {
+    data = data.toString()
+    data = data.split('\n')
+    const labels = ['cpu', 'mem', 'pid', 'time', 'user', 'process']
+    let dbObject = []
+    for (let i = 0; i < data.length - 1; i++) {
+      data[i] = data[i].split(',')
+      let buildArray = []
+      for (let j = 0; j < data[i].length; j++) {
+        buildArray.push({ [labels[j]]: data[i][j] })
+      }
+      dbObject.push(buildArray)
+    }
     //format terminal output
-    dbObject.forEach(row => {
-        log(`${row[0].cpu}\t${row[1].mem}\t${row[2].pid}\t${row[3].time.padEnd(8,' ')}\t${row[4].user.padEnd(8," ")}\t${row[5].process}`)
-        })
+    dbObject.forEach((row) => {
+      log(
+        `${row[0].cpu}\t${row[1].mem}\t${row[2].pid}\t${row[3].time.padEnd(
+          8,
+          ' '
+        )}\t${row[4].user.padEnd(8, ' ')}\t${row[5].process}`
+      )
+    })
     dbObject.shift()
     // log(dbObject)
-   
-    })
-   
+  })
 
-    processSort.stderr.on('data', data => console.error(data.toString))
-    
+  processSort.stderr.on('data', (data) => console.error(data.toString))
 }
 
-export {processList}
+export { processList }
--- index.js
+++ index.js
@@ -1,106 +1,107 @@
-import {cpus, loadavg, freemem, totalmem } from 'node:os';
-import chalk from 'chalk';
-import {processList} from "./csv.js";
-import {log} from 'node:console';
-import sound from 'sound-play';
+import { cpus, loadavg, freemem, totalmem } from 'node:os'
+import chalk from 'chalk'
+import { processList } from './csv.js'
+import { log } from 'node:console'
+import sound from 'sound-play'
 const alert2 = chalk.bgRedBright.whiteBright.bold
 const alert = chalk.bgYellow.bold
 const warn = chalk.yellow.bgBlack
 
 let audioAlertAvailable = true
 async function audioAlert() {
-    function playAlert() {
-        sound.play('audio/alert.mp3', function(err){
-                if (err) throw err
-            })
-    }
-    if (audioAlertAvailable == true) {
-        playAlert()
-        setTimeout(()=>{
-            playAlert()
-        },250)
-        audioAlertAvailable = false
-        setTimeout(()=>{
-            audioAlertAvailable = true
-        },300000)
-    }
-
+  function playAlert() {
+    sound.play('audio/alert.mp3', function (err) {
+      if (err) throw err
+    })
+  }
+  if (audioAlertAvailable == true) {
+    playAlert()
+    setTimeout(() => {
+      playAlert()
+    }, 250)
+    audioAlertAvailable = false
+    setTimeout(() => {
+      audioAlertAvailable = true
+    }, 300000)
+  }
 }
 
 // const avgLoad = setInterval(()=>console.log(loadavg()), 2000)
 // console.log(cpus())
 const getCpuUsage = async () => {
-    const cpuData = cpus()
-    let accumulator = {user:0,sys:0,idle:0}
-    cpuData.forEach((el, i)=> {
-        accumulator.user +=  el.times.user
-        accumulator.sys += el.times.sys
-        accumulator.idle += el.times.idle
-    });
-    const {user, sys, idle} = accumulator
-    const cpuUsed = (user + sys) / (user + sys + idle)
-    return {cpu: cpuUsed.toFixed(4)}
-} 
+  const cpuData = cpus()
+  let accumulator = { user: 0, sys: 0, idle: 0 }
+  cpuData.forEach((el, i) => {
+    accumulator.user += el.times.user
+    accumulator.sys += el.times.sys
+    accumulator.idle += el.times.idle
+  })
+  const { user, sys, idle } = accumulator
+  const cpuUsed = (user + sys) / (user + sys + idle)
+  return { cpu: cpuUsed.toFixed(4) }
+}
 
 const getMemUsage = async () => {
-    const freeMemory = freemem()
-    const totalMem = totalmem()
-    const usedMemory = (totalMem - freeMemory) / totalMem
-    return {usedMem: usedMemory.toFixed(4), freeMem: freeMemory}
+  const freeMemory = freemem()
+  const totalMem = totalmem()
+  const usedMemory = (totalMem - freeMemory) / totalMem
+  return { usedMem: usedMemory.toFixed(4), freeMem: freeMemory }
 }
 
-const systemUsage = async () =>  {
-    const timeStamp = new Date()
-    const cpu = await getCpuUsage()
-    const ram = await getMemUsage()
-    ram.freeMem = (ram.freeMem / 1_000_000).toFixed(2) //convert to megabytes from bytes   
-    return {usedMem: ram.usedMem, freeMem: ram.freeMem, cpu: cpu.cpu, time: timeStamp}
+const systemUsage = async () => {
+  const timeStamp = new Date()
+  const cpu = await getCpuUsage()
+  const ram = await getMemUsage()
+  ram.freeMem = (ram.freeMem / 1_000_000).toFixed(2) //convert to megabytes from bytes
+  return {
+    usedMem: ram.usedMem,
+    freeMem: ram.freeMem,
+    cpu: cpu.cpu,
+    time: timeStamp,
+  }
 }
 
+setInterval(async () => {
+  const report = await systemUsage()
+  const { usedMem, freeMem, cpu, time } = report
+  console.clear()
+  switch (true) {
+    case usedMem > 0.5:
+      // log('start process logging for ram')
+      processList('mem')
+      audioAlert()
+    case usedMem > 0.85:
+      log(alert2(`Ram use getting critical ${usedMem}`))
+      break
+    case usedMem > 0.7:
+      log(alert(`High ram use!! ${usedMem}`))
+      break
+    case usedMem > 0.5:
+      log(warn(`Ram use getting high`))
+      break
+    default:
+      break
+  }
 
-setInterval(async ()=>{
-    const report =  await systemUsage()
-    const {usedMem, freeMem, cpu, time} = report
-    console.clear();
-    switch (true) {
-        case usedMem > .50 :
-            // log('start process logging for ram')
-            processList("mem")
-            audioAlert()
-        case usedMem > .85:
-            log(alert2(`Ram use getting critical ${usedMem}`))
-            break
-        case usedMem > .70 :
-            log(alert(`High ram use!! ${usedMem}`))
-            break
-        case usedMem > .50 :
-            log(warn(`Ram use getting high`))
-            break
-        default:
-            break;
-    }
+  switch (true) {
+    case cpu > 0.5:
+      // log('star process logging for cpu')
+      processList('cpu')
+      audioAlert()
+    case cpu > 0.85:
+      log(alert2(`CPU use getting critical ${cpu}`))
+      break
+    case cpu > 0.7:
+      log(alert(`High CPU use!! ${cpu}`))
+      break
+    case cpu > 0.5:
+      log(warn(`CPU use getting high`))
+      break
+    default:
+      break
+  }
+  // processList("mem")
+  if (cpu > 0.6) log(warn(`high cpu use!! ${cpu}`))
 
-    switch (true) {
-        case cpu > .50 :
-            // log('star process logging for cpu')
-            processList("cpu")
-            audioAlert()
-        case cpu > .85:
-            log(alert2(`CPU use getting critical ${cpu}`))
-            break
-        case cpu > .70 :
-            log(alert(`High CPU use!! ${cpu}`))
-            break
-        case cpu > .50 :
-            log(warn(`CPU use getting high`))
-            break
-        default:
-            break;
-    }
-        // processList("mem")
-    if (cpu > .6) log(warn(`high cpu use!! ${cpu}`))
-    
-    log(report)
-   },2000)
-
-
+  log(report)
+}, 2000)
