f4ab50e1999df8b0b4940c89bef286af42f1a858
Dave May <echo.dave@protonmail.com>
2023-10-28 18:17:11 -0400

    bash script interoperability work on #7

---
+++ scripts/psCPU_unbuntu.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# macos variant 
+#ps -axcro '%cpu,%mem,pid,time,user,comm' | head -n 10 | awk '{print $1","$2","$3","$4","$5","$6" "$7" "$8}'
+
+#unbuntu variant
+ps -axco '%cpu,%mem,pid,time,user,comm' --sort -%cpu | head -n 10 | awk '{print $1","$2","$3","$4","$5","$6" "$7" "$8}'
