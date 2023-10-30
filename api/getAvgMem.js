6d74a8ceb09cc4b531ffc8cf1684703366d6f2e9
Dave May <echo.dave@protonmail.com>
2023-10-30 03:11:49 -0400

    Remove boiler plate

--- api/getAvgMem.js
+++ api/getAvgMem.js
@@ -28,16 +28,8 @@ export default async () => {
     }
   ];
   
-//   const client = await MongoClient.connect(
-    // 'mongodb+srv://aednssm:ekvTwAEtrDFxxgivbwyP8rhQj@cluster0.kz8lrxy.mongodb.net/',
-//     { useNewUrlParser: true, useUnifiedTopology: true }
-//   );
-const result = await telemetry.aggregate(agg)
-  // const coll = db.collection('telemetry');
-  // const cursor = coll.aggregate(agg);
-  // const result = await cursor.toArray();
-  // await client.close();
 
+const result = await telemetry.aggregate(agg)
 
 
 console.log(result)
