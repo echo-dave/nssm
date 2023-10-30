1fa85e60d1c23a6815add95904d4099fba249327
Dave May <echo.dave@protonmail.com>
2023-10-30 02:23:40 -0400

    refactored and logging first successful entry)

--- api/tsData.js
+++ api/tsData.js
@@ -1,11 +1,17 @@
-import db from '../utils/dbcon.js'
+import {client,telemetry} from '../utils/dbcon.js'
+import {log} from 'node:console'
 
+
+
+let logStartup = 1
 export default async (data) => {
   try {
-    let telemetry = await db.collection('telemetry')
     let result = await telemetry.insertOne(data)
-
-    console.log(result)
+    // let result = await client.db(process.env.MONGO_DB).collection('telemetry').insertOne(data)
+    if (logStartup == 1) {
+      log(result)
+      logStartup = 0
+    }
   } catch (e) {
     console.log(e)
   }
