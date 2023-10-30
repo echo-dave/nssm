4a08b41a40170631d6b3325d018c0b6368c483f9
Dave May <echo.dave@protonmail.com>
2023-10-30 02:29:44 -0400

    add a mongo ping to check connection and collection #4

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ import 'dotenv/config'
 import express from 'express'
 import getTelemetry from './utils/getTelemetry.js'
 import {log} from 'node:console'
-import {MongoClient, ServerApiVersion} from 'mongodb'
+import {ping as pingMongo} from './utils/dbcon.js'
+import chalk from 'chalk'
+import getAvgMem from './api/getAvgMem.js'
 const app = express();
 const PORT = process.env.PORT || 3002
 
+
+await pingMongo()
+
 app.listen(PORT, () => {
-    console.log(`NSSM listening on port ${PORT}`)
+    console.log(chalk.yellowBright.bgBlack(`NSSM listening on port ${PORT}`))
   })
 
+
 getTelemetry("headless")
+getAvgMem()
