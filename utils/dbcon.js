6bc81676839f39e2b4755a90bcd6553f9512808d
Dave May <echo.dave@protonmail.com>
2023-10-30 02:27:59 -0400

    refactored, added a check for collections - create if missing #3 #4

--- utils/dbcon.js
+++ utils/dbcon.js
@@ -1,21 +1,64 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
-
+import chalk from 'chalk'
+const info = (string) => console.log(chalk.yellowBright.bgBlack(string))
+const tsCollection = 'ban8'
+const log = (toLog) => console.dir(toLog, { depth: null, colors: true })
 const uri = process.env.MONGO_URI
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-})
+const client = new MongoClient(uri)
+//     {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// })
 
-let conn
-try {
-  conn = await client.connect()
-} catch (e) {
-  console.error(e)
+const findCollection = async (collections) => {
+  info('\nMongo collection found and continuing startup \n')
+  collections.map((el) => {
+    el.name === tsCollection ? log(el) : null
+  })
+  return 'done'
 }
 
-let db = conn.db(process.env.MONGO_DB)
+const initializeDB = async (newCollection) => {
+  try {
+    info("\nMongo collection not found, we'll make one now! \n")
+    await client.db(process.env.MONGO_DB).createCollection(newCollection, {
+      timeseries: {
+        timeField: 'time',
+        metaField: 'meta',
+        //   bucketMaxSpanSeconds: 2,
+        //   bucketRoundingSeconds: 2,
+        granularity: 'seconds',
+      },
+      expireAfterSeconds: 259200,
+    })
+    ping()
+  } catch (e) {
+    console.error(e)
+  }
+}
 
-export default db
+const ping = async () => {
+  try {
+    const collections = await client
+      .db(process.env.MONGO_DB)
+      .listCollections()
+      .toArray()
+
+    const exists = collections.some((el) => el.name === tsCollection)
+    exists === true ? findCollection(collections) : initializeDB(tsCollection)
+    // log(collections)
+    // await client.db(process.env.MONGO_DB).command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (e) {
+    console.error(e)
+  }
+}
+// collections = await client.db(process.env.MONGO_DB).listCollections().toArray();
+
+const db = (dbCollection) =>
+  client.db(process.env.MONGO_DB).collection(dbCollection)
+const telemetry = db('telemetry')
+export { telemetry, db, client, ping }
