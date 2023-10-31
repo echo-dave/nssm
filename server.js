39a03862723a0a97bf2c02de23bcbf2e34dae98b
Dave May <echo.dave@protonmail.com>
2023-10-31 01:21:32 -0400

    updated avg mem query, added test query work #4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import getTelemetry from './utils/getTelemetry.js'
 import {log} from 'node:console'
 import {ping as pingMongo} from './utils/dbcon.js'
 import chalk from 'chalk'
-import getAvgMem from './api/getAvgMem.js'
+import {getAvgMem} from './api/getAvgMem.js'
 const app = express();
 const PORT = process.env.PORT || 3002
 
@@ -16,5 +16,5 @@ app.listen(PORT, () => {
   })
 
 
-getTelemetry("headless")
-getAvgMem()
+getTelemetry('headless')
+// getAvgMem(22)
