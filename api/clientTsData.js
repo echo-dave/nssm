0cd6f7b973429009140e68fcc0e614d054057d79
Dave May <echo.dave@protonmail.com>
2023-10-31 07:06:30 -0400

    code cleanup

--- api/clientTsData.js
+++ api/clientTsData.js
@@ -3,15 +3,8 @@ import dispTelemetry from '../utils/dispTelemetry.js'
 
 
 // change stream unsupported with time series collections - planned for future
-// const changeStream = telemetry.watch()
-// changeStream.on('change', next => {
-//     console.log('change');
-//     console.dir(next,{depth: 2})
-// })
-(async () => {
-const data = await telemetry.findOne({},{projection:{_id:0},sort:{_id: -1}})
-console.log('data: ', data)
-
-dispTelemetry(data)
-
-})()
+const changeStream = telemetry.watch()
+while (await changeStream.hasNext()) {
+    const next = await changeStream.next();
+    dispTelemetry(next.fullDocument)
+}
