006530718f6bfce3ce0cb08880eece4497f26de1
Dave May <echo.dave@protonmail.com>
2023-10-31 06:52:32 -0400

    bug fix - missing a process logline

--- utils/dispProcs.js
+++ utils/dispProcs.js
@@ -6,30 +6,34 @@ const procs = async (data) => {
   let headers = {}
   labels.forEach((el) => (headers[el] = el))
 
-//   sortProcess === 'mem' ? dbObject.unshift(headers) : cpuObject.unshift(headers)
+  //   sortProcess === 'mem' ? dbObject.unshift(headers) : cpuObject.unshift(headers)
 
-const logProcs = (dataSet) => {
+  const logProcs = (dataSet) => {
     log(`Sorting by ${dataSet} useage`)
-  data.processes[dataSet].forEach((row, i) => {
- 
+    data.processes[dataSet].forEach((row, i) => {
+      if (i == 0) {
+        log(
+          chalk.greenBright.underline(
+            `cpu\tmem\tpid\t${'time'.padEnd(8, ' ')}\t${'user'.padEnd(
+              8,
+              ' '
+            )}\tprocess`
+          )
+        )
+      }
+      log(
+        `${row.cpu}\t${row.mem}\t${row.pid}\t${row.time.padEnd(
+          8,
+          ' '
+        )}\t${row.user.padEnd(8, ' ')}\t${row.process}`
+      )
+    })
+  }
 
-    if (i == 0) {
-      log(chalk.greenBright.underline(
-        `cpu\tmem\tpid\t${'time'.padEnd(8,' ')}\t${'user'.padEnd(8, ' ')}\tprocess`
-      ))
-    } else {
-        log(`${row.cpu}\t${row.mem}\t${row.pid}\t${row.time.padEnd(
-        8,
-        ' '
-      )}\t${row.user.padEnd(8, ' ')}\t${row.process}`)
-    }
-  })
-}
+  if (data.processes.mem) logProcs('mem')
+  if (data.processes.cpu) logProcs('cpu')
 
-if (data.processes.mem) logProcs('mem')
-if (data.processes.cpu) logProcs('cpu')
-
-//   return (await sortProcess) === 'mem' ? dbObject : cpuObject
+  //   return (await sortProcess) === 'mem' ? dbObject : cpuObject
 }
 
 export default procs
