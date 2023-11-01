dbdbeb54c31cb15c7aa67aa0a6a562621e8dc29e
Dave May <echo.dave@protonmail.com>
2023-10-31 22:43:02 -0400

    fix #11 client prompt which host to monitor if more than 1 in db

--- api/clientTsData.js
+++ api/clientTsData.js
@@ -3,8 +3,19 @@ import dispTelemetry from '../utils/dispTelemetry.js'
 
 
 // change stream unsupported with time series collections - planned for future
-const changeStream = telemetry.watch()
-while (await changeStream.hasNext()) {
-    const next = await changeStream.next();
-    dispTelemetry(next.fullDocument)
+const getClientData = async (hostname) => {
+    console.log('getting data: ', hostname)
+    const pipeline = []
+    if (hostname?.length > 1) {
+    pipeline.push({$match: {'fullDocument.meta.hostname': hostname}})
+    }
+    try {
+    const changeStream = telemetry.watch(pipeline)
+    while (await changeStream.hasNext()) {
+        const next = await changeStream.next();
+        dispTelemetry(next.fullDocument)
+    }
+    } catch(e) {console.error(e)}
 }
+
+export default getClientData
