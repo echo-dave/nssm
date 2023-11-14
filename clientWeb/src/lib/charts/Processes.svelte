12697c847a345d7f86c7619c676525d46d411c84
Dave May <echo.dave@protonmail.com>
2023-11-14 04:29:17 -0500

    work on #14 fix bar chart stacking, chart fonts

--- clientWeb/src/lib/charts/Processes.svelte
+++ clientWeb/src/lib/charts/Processes.svelte
@@ -1,48 +1,64 @@
 <script>
+  import { browser } from '$app/environment'
   import { Bar } from 'svelte-chartjs'
   import { Chart as ChartJS, Title, Tooltip, Legend, BarElement } from 'chart.js'
   import options from '$lib/charts/processesOptions.js'
-
   ChartJS.register(Title, Tooltip, Legend, BarElement)
-
+  ChartJS.defaults.font.family = "'Montserrat-light'"
+  ChartJS.defaults.font.weight = 'normal'
   export let metrics
   let chartData
   let skip = true
 
-  function updateData(metrics) {
-    skip = !skip
-    if (skip === true) return
-    metrics?.at(-1)?.processes.cpu.sort((a, b) => {
-      return a.process.toUpperCase() < b.process.toUpperCase() ? -1 : 1
-    })
-
-    chartData = {
-      title: 'Processes',
-      // labels: metrics?.at(-1)?.processes.cpu.map((x) => x.process),
-      datasets: [
-        {
-          backgroundColor: [
-            '#005C56',
-            '#003f5c',
-            '#2f4b7c',
-            '#665191',
-            '#a05195',
-            '#d45087',
-            '#f95d6a',
-            '#ff7c43',
-            '#ffa600'
-          ],
-          parsing: { xAxisKey: 'process', yAxisKey: 'cpu' },
-          label: '',
-          data: metrics?.at(-1)?.processes.cpu
-          // data: metrics?.at(-1)?.processes.cpu.map((x) => x.cpu)
+  async function updateData(metrics) {
+    try {
+      chartData = await new Promise(async (res, rej) => {
+        if (!metrics?.at(-1)?.processes?.cpu) {
+          rej(new Error('No metrics data'))
         }
-      ]
+        skip = !skip
+        if (skip === true) return
+        metrics?.at(-1)?.processes.cpu.sort((a, b) => {
+          return a.process.toUpperCase() < b.process.toUpperCase() ? -1 : 1
+        })
+        res({
+          title: 'Processes',
+          labels: metrics?.at(-1)?.processes.cpu.map((x) => x.process),
+          datasets: [
+            {
+              backgroundColor: [
+                '#005C56',
+                '#003f5c',
+                '#2f4b7c',
+                '#665191',
+                '#a05195',
+                '#d45087',
+                '#f95d6a',
+                '#ff7c43',
+                '#ffa600'
+              ],
+              parsing: { xAxisKey: 'process', yAxisKey: 'cpu' },
+
+              data: metrics?.at(-1)?.processes.cpu
+              // data: metrics?.at(-1)?.processes.cpu.map((x) => x.cpu)
+            }
+          ]
+        })
+      })
+    } catch (e) {
+      console.error(e)
     }
   }
-  $: updateData(metrics)
+  $: if (metrics) updateData(metrics)
 </script>
 
-{#if JSON.stringify(metrics) !== ''}
+{#await metrics && chartData then}
   <Bar data={chartData} {options} />
-{/if}
+{/await}
+
+<style>
+  @font-face {
+    font-family: 'Montserrat-light';
+    src: url('$lib/fonts/Montserrat-light.ttf');
+  }
+</style>
--- clientWeb/src/lib/charts/dataOptions.js
+++ clientWeb/src/lib/charts/dataOptions.js
@@ -6,16 +6,27 @@ const options = {
     legend: {
       align: 'center',
       display: true,
-      Title: { diplay: true, color: 'white' },
+
+      Title: { display: false, color: 'white', font: { family: 'Montserrat-Light' }, size: 12 },
       labels: {
         color: 'white',
-        usePointStyle: true
+        usePointStyle: true,
+        font: {
+          family: 'Montserrat-Light',
+          wight: 'light'
+        }
       }
     }
   },
   scales: {
     x: {
       ticks: {
+        font: {
+          family: 'Montserrat-light, Verdana, Geneva, Tahoma, sans-serif',
+          size: 12,
+          weight: 'light'
+        },
+
         // For a category axis, the val is the index so the lookup via getLabelForValue is needed
         callback: function (val) {
           // Hide every 2nd tick label
