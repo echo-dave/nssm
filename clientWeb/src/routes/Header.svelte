7e84892386c88894743bed8131d53af7b51a4cae
Dave May <echo.dave@protonmail.com>
2023-11-14 04:42:27 -0500

    work on #14 menu for server select

--- clientWeb/src/routes/Header.svelte
+++ clientWeb/src/routes/Header.svelte
@@ -1,8 +1,36 @@
 <script>
   import CurrentUsage from '$lib/charts/CurrentUsage.svelte'
+  import { onMount } from 'svelte'
 
   export let metrics
-  $: currentTime = metrics.at(-1)?.time.toLocaleString('default', {
+  let hostmenutoggle = false
+  let currentHost
+  //storing a pending promise because svelt needs to await data from the onMount
+  let hostnames = new Promise(() => {})
+  onMount(() => {
+    hostnames = getNames().then((res) => {
+      hostnames = res
+    })
+  })
+
+  const getNames = async () => {
+    let response = await fetch('/api/gethostnames')
+    if (!response.ok) {
+      throw new Error('Hostnames not available')
+    }
+    response = await response.json()
+    currentHost = response.pop()
+    console.log(response)
+    return response
+  }
+
+  const changeServer = async (name) => {
+    console.log(name)
+    let resp = await fetch(`/api/serverchange/${name}`)
+    resp = await resp.json()
+    console.log(resp)
+  }
+  $: currentTime = metrics?.at(-1)?.time.toLocaleString('default', {
     year: 'numeric',
     month: 'short',
     day: '2-digit',
@@ -11,27 +39,70 @@
     second: '2-digit',
     hour12: false
   })
-  let buildChart = new Promise(async (res, rej) => {
-    await metrics
-    res([
-      { metrics: metrics.at(-1)?.usedMem, labelChart: 'MEM', titleText: 'Used MEM' },
-      { metrics: metrics.at(-1)?.cpu, labelChart: 'CPU', titleText: 'Used CPU' }
-    ])
-  })
-  buildChart.then((response) => {
-    buildChart = response
-  })
+  let buildChart
+  async function build(metrics) {
+    buildChart = new Promise(async (res, rej) => {
+      await metrics
+      res([
+        {
+          metrics: metrics?.at(-1)?.usedMem,
+          labelChart: 'MEM',
+          titleText: 'Used MEM',
+          color: 'rgb(178,52,107)'
+        },
+        {
+          metrics: metrics?.at(-1)?.cpu,
+          labelChart: 'CPU',
+          titleText: 'Used CPU',
+          color: 'rgb(57,177,177)'
+        }
+      ])
+      // console.log(metrics.at(-1))
+    })
+    buildChart.then((response) => {
+      buildChart = response
+    })
+  }
+  $: visibilityToggle = hostmenutoggle ? 'visible' : 'hidden'
+
+  $: build(metrics)
 </script>
 
-<!-- <flexbox> -->
 <div id="liveSysData">
-  <!-- flex  -->
   <div class="no-border" style="flex: 1 1;">
-    <span class="hostname">{metrics.at(-1)?.meta.hostname}</span>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <span
+      class="hostname"
+      on:click={() => {
+        hostmenutoggle = !hostmenutoggle
+      }}>{metrics?.at(-1)?.meta.hostname}</span
+    >
+    {#if true}
+      <ul id="hostnames" style="visibility: {visibilityToggle}">
+        {#await hostnames then hostnames}
+          {#each hostnames as name}
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+            <li
+              on:click={(e) => {
+                changeServer(name)
+                hostmenutoggle = false
+              }}
+            >
+              {name}
+            </li>
+          {/each}
+        {:catch error}
+          <span style="color: red; padding: 1em 0; display:inline-block">{error}</span>
+        {/await}
+      </ul>
+    {/if}
     <div class="meters no-border">
       {#await buildChart then}
         {#each buildChart as chart}
           <div class="currentUsage no-border">
+            <div class="digitDisplay no-border">%<br />{(chart.metrics * 100).toFixed(2)}</div>
             <CurrentUsage {...chart} />
           </div>
         {/each}
@@ -39,20 +110,24 @@
     </div>
   </div>
   <ul id="topMetrics" style="flex: 1 1;">
-    <li><h2>Simple <br />System Monitor</h2></li>
-
-    <!-- <li>Cpu:{(metrics.at(-1)?.cpu * 100).toFixed(2)}%</li>
-      <li>Mem:{(metrics.at(-1)?.usedMem * 100).toFixed(2)}%</li> -->
+    <li><h1>Simple <br />System Monitor</h1></li>
     <li>
-      Mem Reported: {metrics.at(-1)?.totalMem}M
+      Mem Reported: {metrics?.at(-1)?.totalMem}M
     </li>
     <li style="color: magenta;">
       {currentTime}
     </li>
   </ul>
 </div>
-
-<!-- </flexbox> -->
+<svelte:window
+  on:click={(e) => {
+    e.target != document.querySelector('#hostnames') &&
+    e.target != document.querySelector('.hostname')
+      ? (hostmenutoggle = false)
+      : null
+    console.log('click')
+  }}
+/>
 
 <style>
   #liveSysData {
@@ -65,20 +140,35 @@
     margin-left: 1em;
     padding-top: 0.5em;
     display: inline-block;
+    cursor: pointer;
   }
+  #hostnames {
+    position: absolute;
+    z-index: 2;
+    background: rgba(0, 0, 0, 0.85);
+    padding: 0.5em 2.5em;
+    border-radius: 0.5em;
+    left: 1.5em;
+    top: 3em;
+    cursor: pointer;
+    visibility: visible;
+    display: inline-block;
+  }
+
+  #hostnames li:hover {
+    color: aqua;
+  }
+  #hostnames li:hover::before {
+    content: '•';
+    margin-left: -0.5em;
+  }
+
   .meters {
     display: flex;
     flex-direction: row;
   }
   .meters div {
     flex: 0 1;
-  }
-  flexbox {
-    display: flex;
-    flex-direction: row-reverse;
-    justify-content: flex-end;
-    gap: 2em;
-    margin-bottom: 0.5em;
   }
 
   #topMetrics {
@@ -91,18 +181,24 @@
   li {
     margin-left: -1.5em;
   }
-
+  #topMetrics :not(:first-child) {
+    margin-bottom: 0.15em;
+  }
   .currentUsage {
     width: 150px;
     height: 150px;
+    position: relative;
+  }
+  .digitDisplay {
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    margin-top: calc(50% - 0.5em);
+    font-size: 0.7em;
+    margin-left: 0.5em;
   }
 
   @media screen and (max-width: 720px) {
-    flexbox {
-      flex-direction: column;
-      gap: 0.5em;
-      justify-content: flex-start;
-    }
     #liveSysData {
       flex-direction: column-reverse;
       height: auto;
