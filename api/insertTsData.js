824d5c4b25c748ffb9fa5b00f4b73d68b7c2d7da
Dave May <echo.dave@protonmail.com>
2023-11-14 11:56:17 -0500

    code cleanup

--- api/insertTsData.js
+++ api/insertTsData.js
@@ -6,11 +6,11 @@ export default async (data) => {
   try {
     let result = await telemetry.insertOne(data)
     // let result = await client.db(process.env.MONGO_DB).collection('telemetry').insertOne(data)
-    if (logStartup == 1) {
+    if (logStartup === 1) {
       log(result)
       logStartup = 0
     }
   } catch (e) {
-    console.log(e)
+    console.error({ error: e, msg: 'Failed to insert data' })
   }
 }
